9434b0c17f25c93cfe7a372705e3b0c3d23add23
VCarbonell <v.carbonell+gh@hotmail.fr>
2022-09-30 15:35:37 +0200

    adding contact section on mobile

--- main.js
+++
@@ -1,8 +0,0 @@
-//Addind form when click on contact on mobile version
-
-function addForm(buttonClicked, event) {
-    event.preventDefault();
-    const form = document.createElement("form");
-    form.classList.add("contactForm")
-    buttonClicked.appendChild(form);
-}
