c2e2c70399f0c9706e4626d2f091549d2f242c77
RYAN_TAMA <UNIQUELYTAMA9@gmail.com>
2022-10-05 12:48:12 +0200

    desktop version

--- profil.js
+++ profil.js
@@ -4,6 +4,14 @@ let profileTitle = document.querySelector("#profile-name");
 let profilePic = document.querySelector("#profile-image");
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
+const profileCard = document.querySelector(".profile-container");
+
+for (let i = 0; i < scroll.length; i++) {
+  scroll[i].onclick = function () {
+    profileTitle.innerText = scroll[i].querySelector(".nameWilder").innerText;
+    profilePic.src = scroll[i].querySelector(".picture").src;
+  };
+}
 
 let i = 0;
 rightArrow.onclick = function () {
