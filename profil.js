5ae17441a2a70fb43023e073bff00974f4574bbe
RYAN_TAMA <UNIQUELYTAMA9@gmail.com>
2022-10-05 09:46:32 +0200

    mobile done

--- profil.js
+++ profil.js
@@ -5,12 +5,6 @@ let profilePic = document.querySelector("#profile-image");
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
 
-for (let i = 0; i < scroll.length; i++) {
-  scroll[i].onclick = function () {
-    profileTitle.innerText = scroll[i].querySelector(".nameWilder").innerText;
-    profilePic.src = scroll[i].querySelector(".picture").src;
-  };
-}
 let i = 0;
 rightArrow.onclick = function () {
   profileTitle.innerText = scroll[i].querySelector(".nameWilder").innerText;
